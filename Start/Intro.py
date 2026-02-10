1dad1d1c63c701f5c9995e715306efa39e81790d
Thái Thuận <thuanntk25416@st.uel.edu.vn>
2026-02-10 16:12:44 +0700

    Add guidance comment for git pull usage
    Added a comment for guidance on using git pull.

--- Start/Intro.py
+++ Start/Intro.py
@@ -1,2 +1,3 @@
 print("Hello anh em, đây là dự án cuối kỳ môn KTLT của nhóm mình")
 print("Dòng này là dòng tao viết khi sửa file trực tiếp trên github")
+#Anh em xem vid hướng dẫn, chỉ cần nhấn git pull là đẩy file về được
